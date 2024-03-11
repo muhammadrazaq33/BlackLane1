b7c7e07989b7fc77da97f373a4a27188c9063227
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-11 19:30:30 +0500

    addED

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import './App.css'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import { Home } from './Components/Home/Home'
-import CityToCity from './pages/cityToCity'
-import AirportTransfer from './Components/AirportTransfer/AirportTransfer'
-import HourlyHire from './Components/HourlyHire/HourlyHire'
-import ChoufferServices from './Components/ChoufferServices/ChoufferServices'
-import LimousineServices from './Components/LimousineServices/LimousineServices'
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Home } from "./Components/Home/Home";
+import CityToCity from "./pages/cityToCity";
+import AirportTransfer from "./components/AirportTransfer/AirportTransfer";
+import HourlyHire from "./components/HourlyHire/HourlyHire";
+import ChoufferServices from "./components/ChoufferServices/ChoufferServices";
+import LimousineServices from "./components/LimousineServices/LimousineServices";
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/city-to-city' element={<CityToCity />}></Route>
-          <Route path='/airportTransfer' element={<AirportTransfer />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/city-to-city" element={<CityToCity />}></Route>
+          <Route path="/airportTransfer" element={<AirportTransfer />}></Route>
           <Route path="/hourlyHire" element={<HourlyHire />} />
           <Route path="/choufferservices" element={<ChoufferServices />} />
           <Route path="/limousineservices" element={<LimousineServices />} />
-
-
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
